ef140da0262c1a1bebf4730994b70ed4564d635b
yamidart <yamidart777@gmail.com>
2024-10-09 15:59:07 +0300

    add files

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Yuryi"
+print("name:", name)
+age = 48
+print("age:", age)
+age = age + 2 #  Перезапишите в age текущее значение переменной age + новое.
+# перезаписано значение age + новое, тут ведь не надо тип менять.
+print("age:", age)
+is_student = True
+print("is_student:", is_student)
